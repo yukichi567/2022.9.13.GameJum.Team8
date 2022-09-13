91ca5420b07dff4df31dbb17ff76972ed5724d69
Yukichi567 <yukichi0567@gmail.com>
2022-09-13 11:37:41 +0900

    マネージャー追加

--- Assets/Marimori/Gamemanager.cs
+++ Assets/Marimori/Gamemanager.cs
@@ -1,9 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine. UI;
 
 public class Gamemanager : MonoBehaviour
 {
+
+    [SerializeField]
+    [Header("プレイ時間の初期値")]
+    private float _GameOverTime = 60f;
+
+
+    [SerializeField]
+    [Header("時間表示")]
+     Text _timerlimit;
+
+    [SerializeField]
+    [Header("スコア")]
+    public float _score;
+
+    [SerializeField]
+    [Header("スコア表示")]
+    Text _Point;
+
+
+    public float _time;
+    public float _count;
+    public float _timelimit = 60;
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +38,13 @@ public class Gamemanager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _time += Time.deltaTime;
+        _count = _GameOverTime - _time;
+        _timerlimit.text = $"{_count.ToString("F1")}";
+    }
+    
+    void AddScore(int _score)
+    {
+
     }
 }
