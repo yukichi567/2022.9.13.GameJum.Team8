ea9aa66953d26ce9ede0ed8296a4a044107c841f
umenokoudai <umenokoudai0805@gmail.com>
2022-09-13 13:04:46 +0900

    エネミースクリプトとクリエイトスクリプト追加

--- Assets/Main/Create.cs
+++ Assets/Main/Create.cs
@@ -2,29 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider))]
 public class Create : MonoBehaviour
 {
     [SerializeField] GameObject[] _objectPrefab;
-    BoxCollider2D _bc;
+    BoxCollider _bc;
     float _timer;
-    float _intarval;
+    [SerializeField]float _intarval;
     float _x;
-    float _y;
+    //float _y;
     int n;
 
     // Start is called before the first frame update
     void Start()
     {
-        _bc = GetComponent<BoxCollider2D>();
+        _bc = GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        _timer += Time.deltaTime;
         _x = Random.Range((-_bc.size.x) / 2, (_bc.size.x) / 2);
         //_y = Random.Range((-_bc.size.y) / 2, (_bc.size.y) / 2);
-        n = Random.Range(0, _objectPrefab.Length);
-        GameObject Enemy = Instantiate(_objectPrefab[n]);
-        Enemy.transform.position = new Vector3(_x, _bc.transform.position.y, _bc.transform.position.z);
+        if (_timer >= _intarval)
+        {
+            n = Random.Range(0, _objectPrefab.Length);
+            GameObject Enemy = Instantiate(_objectPrefab[n]);
+            Enemy.transform.position = new Vector3(_x, _bc.transform.position.y, _bc.transform.position.z);
+            _timer = 0;
+        }
     }
 }
