e504e7bc119b6f9c95e7a3542aa2417c64215146
umenokoudai <umenokoudai0805@gmail.com>
2022-10-12 15:33:05 +0900

    Help追加

--- Assets/Hioki/ScenSquare.cs
+++ Assets/Hioki/ScenSquare.cs
@@ -8,6 +8,10 @@ public class ScenSquare : MonoBehaviour
     /// <summary>ïœÇ¶ÇΩÇ¢ÉVÅ[ÉìÇÃñºëO</summary>
     [SerializeField] string _changeScene;
     [SerializeField] GameObject _clickAudio;
+    [SerializeField] GameObject _open;
+    [SerializeField] GameObject _close;
+    [SerializeField] GameObject _open2;
+    [SerializeField] GameObject _close2;
 
     public void LoadScenes()
     {
@@ -18,5 +22,19 @@ public class ScenSquare : MonoBehaviour
         Instantiate(_clickAudio);
         yield return new WaitForSeconds(0.1f);
         SceneManager.LoadScene(_changeScene);
+    }
+    public void SetActive()
+    {
+        _open.gameObject.SetActive(true);
+        _close.gameObject.SetActive(false);
+        Instantiate(_clickAudio);
+    }
+    public void SetActiveHelp()
+    {
+        _open.gameObject.SetActive(true);
+        _close.gameObject.SetActive(false);
+        _open2.gameObject.SetActive(true);
+        _close2.gameObject.SetActive(false);
+        Instantiate(_clickAudio);
     }
 }
